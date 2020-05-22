5a223990c747c8849ea607084a576e5330e6d067
Maftuna Khakimova <sobira204@gmail.com>
2020-05-22 15:55:46 +0500

    start work add class Game

---
+++ Models/Games.cs
@@ -0,0 +1,10 @@
+namespace FirstWebApp.Models
+{
+    public class Games
+    {
+        public int Id{get;set;}
+        public string NameOfGame{get;set;}
+        public long Players{get;set;}
+        public double Mark{get;set;}
+    }
+}
